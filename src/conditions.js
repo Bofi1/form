2f60cb883d5861416ad31fc9089152764826dfe9
bofi1 <angelborque03github@gmail.com>
2024-07-05 21:30:07 -0700

    practicando validacion

--- src/conditions.js
+++ src/conditions.js
@@ -1,6 +1,10 @@
 
 let firstName = document.getElementById('firstName')
 let titleFirstName = document.getElementById('titleFirstName')
+
+let titleLastName = document.getElementById('titleLastName')
+let lastName = document.getElementById('lastName')
+
 let Email
 let query
 let msj
@@ -10,9 +14,8 @@ let boton = document.getElementById('submit')
 
 
 
-firstN = () => {
+firstN = () => { // first name
 
-    
         if (firstName.value != '') { // first name
     
             titleFirstName.classList.remove('text-[red]');
@@ -20,7 +23,7 @@ firstN = () => {
     
             firstName.classList.remove('border-[red]');
             firstName.classList.add('border-black');
-            
+    
         } else {
     
             titleFirstName.classList.remove('text-black');
@@ -29,6 +32,24 @@ firstN = () => {
             firstName.classList.remove('border-black');
             firstName.classList.add('border-[red]');
         }
+}
+
+lastN = () => {
+    if (lastName.value != '') {
+
+        titleLastName.classList.remove('text-[red]');
+        titleLastName.classList.add('text-black');
+
+        lastName.classList.remove('border-[red]');
+        lastName.classList.add('border-black');
+        
+    } else {
+        titleLastName.classList.remove('text-black');
+        titleLastName.classList.add('text-[red]');
+
+        lastName.classList.remove('border-black');
+        lastName.classList.add('border-[red]');
+    }
 }
 
 
